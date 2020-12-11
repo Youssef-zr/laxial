2d798e9184dc14f71a5a076fc3edf44f4d12128b
youssef <yn-neinaa@hotmail.com>
2020-12-11 15:10:14 +0100

    dashboard

--- resources/views/website/layouts/header.blade.php
+++ resources/views/website/layouts/header.blade.php
@@ -8,8 +8,8 @@
     <title>
        Laxial Offer
     </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{url('css/app.css')}}">
     <link rel="stylesheet" href="{{url('plugins/particles/demo/css/style.css')}}">
     <link rel="icon" href="/images/laxial.ico">
