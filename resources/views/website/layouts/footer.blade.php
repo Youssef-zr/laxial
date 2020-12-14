829bba0fd2144ad34c8d0808c89d66a11b1b0901
youssef <yn-neinaa@hotmail.com>
2020-12-14 09:45:14 +0100

    https

--- resources/views/website/layouts/footer.blade.php
+++ resources/views/website/layouts/footer.blade.php
@@ -3,9 +3,9 @@
 </div>
 
 <!-- ------------- scripts resources --------------- -->
-<script src="http://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script src="{{url('js/app.js')}}"></script>
 <script src="{{url('js/main.js')}}"></script>
 <script src="{{url('plugins/particles/particles.js')}}"></script>
