a6acf88f8d7cc668ba5fb1cc0c70d51d8739a095
youssef <yn-neinaa@hotmail.com>
2020-12-16 16:54:31 +0100

    fix client ip

--- app/Http/Controllers/Api/OffersController.php
+++ app/Http/Controllers/Api/OffersController.php
@@ -111,7 +111,21 @@ class OffersController extends Controller
     public function localisation(Request $request )
     {
         $ip = $_SERVER['REMOTE_ADDR']; //For static IP address get
-        $location = Location::get(".$ip.");
+        $location =$this->getRealIpAddr();
         return response()->json(['localisation' => $location]);
     }
+
+    function getRealIpAddr(){
+        if ( !empty($_SERVER['HTTP_CLIENT_IP']) ) {
+         // Check IP from internet.
+         $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+         // Check IP is passed from proxy.
+         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+         // Get IP address from remote address.
+         $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
+       }
 }
