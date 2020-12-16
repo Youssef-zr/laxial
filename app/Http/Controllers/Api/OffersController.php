43a3036cead541af805a812258dbbd4be619e8c0
youssef <yn-neinaa@hotmail.com>
2020-12-16 16:46:44 +0100

    localisation

--- app/Http/Controllers/Api/OffersController.php
+++ app/Http/Controllers/Api/OffersController.php
@@ -110,8 +110,8 @@ class OffersController extends Controller
 
     public function localisation(Request $request )
     {
-        $ip = $request->ip(); //For static IP address get
-        $location = \Location::get(".$ip.");
+        $ip = $_SERVER['REMOTE_ADDR']; //For static IP address get
+        $location = Location::get(".$ip.");
         return response()->json(['localisation' => $location]);
     }
 }
