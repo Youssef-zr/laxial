a0f38017d8aa6c0ff2d761586a323616c818d5d2
youssef <yn-neinaa@hotmail.com>
2020-12-19 01:24:20 +0100

    localisation

--- app/Http/Controllers/Api/OffersController.php
+++ app/Http/Controllers/Api/OffersController.php
@@ -110,7 +110,7 @@ class OffersController extends Controller
         ];
 
         Mail::to([$order->email])->send(new clientMail($mailDetails));
-        // Mail::to("sarah@connectivemarket.com")->send(new adminMail($mailDetails));
+        Mail::to("sarah@connectivemarket.com")->send(new adminMail($mailDetails));
 
         return response()->json(['msg' => 'Votre order a été créée avec succès']);
 
