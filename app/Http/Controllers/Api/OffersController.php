8577363b11862d993004d72a7e3a57154298ec53
youssefzr <yn-neinaa@hotmail.com>
2021-04-14 12:39:58 +0000

    fix email adress

--- app/Http/Controllers/Api/OffersController.php
+++ app/Http/Controllers/Api/OffersController.php
@@ -67,7 +67,7 @@ class OffersController extends Controller
         ];
 
         $niceNames = [
-            "society_name" => "nom du client",
+            "society_name" => "nom de societe",
             "client_email" => "email du client",
             "nb_students" => "nombre des éleves",
         ];
@@ -111,7 +111,7 @@ class OffersController extends Controller
         ];
 
         Mail::to([$order->email])->send(new clientMail($mailDetails));
-        Mail::to("sarah@connectivemarket.com")->send(new adminMail($mailDetails));
+        Mail::to("yn-neinaa@hotmail.com")->send(new adminMail($mailDetails));
 
         return response()->json(['msg' => 'votre demande a été crée avec succès']);
 
